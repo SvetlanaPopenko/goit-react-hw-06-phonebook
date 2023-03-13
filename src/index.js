6cbb5667d1ed6c3fb8dd596d9ff8f8a5882aeab0
SvetlanaPopenko <spopenko02@gmail.com>
2023-03-14 00:51:29 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
@@ -5,7 +6,9 @@ import { Provider } from 'react-redux';
 import { store } from 'redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
   <Provider store={store}>
     <App />
-  </Provider>
+    </Provider>
+    </React.StrictMode>
 );
