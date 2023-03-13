4a0f7426240af34be7654b0f358707541cba5ece
SvetlanaPopenko <spopenko02@gmail.com>
2023-03-13 19:54:57 +0200

    Update ContactList.styled.jsx

--- src/components/ContactList/ContactList.styled.jsx
+++ src/components/ContactList/ContactList.styled.jsx
@@ -12,7 +12,7 @@ export const ContactsList = styled.ul`
   padding: 10px;
 `;
 
-export const ContactItem = styled.li`
+export const ContactItems = styled.li`
   display: flex;
   font-size: 30px;
   font-weight: 500;
@@ -23,25 +23,3 @@ export const ContactItem = styled.li`
   background: linear-gradient(160deg, #f0ade4, #d108ec);
 `;
 
-export const ContactListButton = styled.button`
-  font-size: 24px;
-  color: white;
-  margin-left: auto;
-  border-radius: 23%;
-  border: inherit;
-  background: linear-gradient(160deg, #ed8bf8, #6d0462);
-  padding: 10px;
-  cursor: pointer;
-  transition: 250ms transform ease-in-out, 250ms box-shadow ease-out,
-    250ms color ease-in;
-  :hover {
-    transform: scale(1.05);
-    font-size: 28px;
-    color: #f8f856;
-    box-shadow: 1px 3px 2px blanchedalmond, -1px -3px 5px #ed8bf8;
-  }
-`;
-
-export const ContactNumber = styled.span`
-  font-weight: 600;
-`;
