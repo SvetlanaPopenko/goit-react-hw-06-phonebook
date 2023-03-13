d1ab1ae88ac7952bce669ebe5efafc98520c0ea5
SvetlanaPopenko <spopenko02@gmail.com>
2023-03-14 00:51:36 +0200

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,10 @@
-import { createStore } from 'redux';
-import { devToolsEnhancer } from '@redux-devtools/extension';
+import { configureStore } from '@reduxjs/toolkit';
+import { contactsReducer } from './contactsSlice';
+import { filterReducer } from './filterSlice';
 
-const initialState = {
-    contacts: [],
-    filters:[],
-}
-const rootReducer = (state = initialState, action) => {
-    return state;
-}
- 
-const enhancer = devToolsEnhancer();
-export const store = createStore(rootReducer, enhancer);
+export const store = configureStore({
+  reducer: {
+    contacts: contactsReducer,
+    filter: filterReducer,
+  },
+});
