64630154b06f48ef4d98dac2680c69dcde0219fe
SvetlanaPopenko <spopenko02@gmail.com>
2023-03-13 19:23:19 +0200

    Create store.js

---
+++ src/redux/store.js
@@ -0,0 +1,13 @@
+import { createStore } from 'redux';
+import { devToolsEnhancer } from '@redux-devtools/extension';
+
+const initialState = {
+    contacts: [],
+    filters:[],
+}
+const rootReducer = (state = initialState, action) => {
+    return state;
+}
+ 
+const enhancer = devToolsEnhancer();
+export const store = createStore(rootReducer, enhancer);
