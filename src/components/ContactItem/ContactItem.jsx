ff1852bb80ef88388663028141db40fdb1dafd55
SvetlanaPopenko <spopenko02@gmail.com>
2023-03-13 19:54:48 +0200

    Create ContactItem.jsx

---
+++ src/components/ContactItem/ContactItem.jsx
@@ -0,0 +1,27 @@
+import { useDispatch } from "react-redux"
+import { deleteContact } from "redux/actions";
+import { ContactNumber, ContactListButton } from "./ContactItem.styled";
+import PropTypes from 'prop-types';
+
+const ContactItem = ({ id, name, number }) => {
+    const dispatch = useDispatch();
+    const handleDeleteContact = () => dispatch(deleteContact(id));
+    return (
+        <div>
+         <p>
+          {name}: <ContactNumber>{number}</ContactNumber>
+        </p>
+        <ContactListButton onClick={handleDeleteContact} >
+          Delete
+            </ContactListButton>
+            </div>
+    );
+};
+
+ContactItem.propTypes = {
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    number: PropTypes.string.isRequired,
+};
+
+export  default ContactItem;
